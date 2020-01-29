a12b258764003a2fe445c38aafdd96c0e217723d
afd <afd@alup5.academiapostal.es.local>
2020-01-29 19:34:56 +0100

    imagenes

---
+++ models/products.js
@@ -0,0 +1,10 @@
+module.exports=[
+    
+        {name:"microondas", price:200, stock:6, imagen: "micro.jpg"},
+        {name:"tele", price:600, stock:3, imagen:"tele.jpg"},
+        {name:"lampara", price:20, stock:14, imagen:"lampara.jpg"},
+        {name:"secadora", price:180, stock:5, imagen:"secadora.jpg"},
+        {name:"lavadora", price:290, stock:3, imagen:"lava.jpg"},
+        {name:" libro javiscript", price:50, stock:0, imagen:"java.jpg"}
+    
+];
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var products=require("../models/products.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const products= [
-    {name:"microondas", price:200, stock:6},
-    {name:"tele", price:600, stock:3},
-    {name:"lampara", price:20, stock:14},
-    {name:"secadora", price:180, stock:5},
-    {name:"lavadora", price:290, stock:3},
-    {name:"cd", price:180, stock:0}
-
-  ];
   res.render('index', { title: 'clase333', products });
 });
 
