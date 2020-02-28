737ed307def40f57ee5a3accd85ecf1173b36771
afd <afd@alup5.academiapostal.es.local>
2020-02-28 19:23:28 +0100

    sync

--- models/index.js
+++ models/index.js
@@ -3,16 +3,28 @@ const Usuario=require('./users');
 const Pedido=require('./pedido');
 const Carrito=require('./carrito');
 const sequelize = require('./db');
-
+Usuario.hasOne(Carrito);
+Carrito.belongsTo(Usuario);
+// Usuario.hasMany(Pedido);
+// Pedido.belongsTo(Usuario);
+// Producto.belongsToMany(Carrito);
+// Producto.belongsToMany(Pedido);
+// Producto.belongsToMany(Carrito);
+// Pedido.hasMany(Usuario);
+// Pedido.belongsTo(Usuario);
+// Producto.hasMany(Pedido);
+// Pedido.belongsToMany(Producto);
 //Conectamos con la BD
 sequelize
+ 
   .authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
-    sequelize.sync();
+    sequelize.sync({alter:true});
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+ 
   module.exports={Producto, Usuario,Pedido,Carrito
   }
--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const sequelize=require('./db');
 //Definimos modelo para producto
 const users = sequelize.define('usuario', {
     nombre: Sequelize.STRING(50),
-    apellidos: Sequelize.STRING(50),
+    apellidos: Sequelize.STRING(80),
     email: Sequelize.STRING(50),
     pass: Sequelize.STRING(25)
     });
